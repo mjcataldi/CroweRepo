f04282beb9fd36367613665b2c5fdb1e28296745
Matthew Cataldi <mcataloe@gmail.com>
2019-11-13 18:14:32 -0600

    Add project files.

---
+++ CroweConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CroweClassLibrary;
+using static System.Console;
+
+namespace CroweConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
